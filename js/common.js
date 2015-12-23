5c6d4eb8b50c730fd35c3383961611076a1ddea3
Ex-Zy <serega1905@ukr.net>
2015-12-23 15:53:06 +0200

    upd datepicker

--- js/common.js
+++ js/common.js
@@ -115,7 +115,10 @@ head.ready(function() {
 	});
 	
 	$("#datepicker").datepicker({
-		dateFormat: "dd.mm"
+		dateFormat: "dd.mm",
+		monthNames: ['Январь', 'Февраль', 'Март', 'Апрель','Май', 'Июнь', 'Июль', 'Август', 'Сентябрь','Октябрь', 'Ноябрь', 'Декабрь'],
+		dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+		firstDay: 1,
 	});
 
 	$('.js-calendar').click(function() {
